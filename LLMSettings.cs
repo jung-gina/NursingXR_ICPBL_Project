06dbb205cac22a2ad0acaf1d7094568a43a7262f
지나 정 <71510068+jung-gina@users.noreply.github.com>
2024-12-17 15:36:57 +0900

    Delete LLMSettings.cs

--- LLMSettings.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "LLMSettings", menuName = "LLM Tools/Settings")]
-public class LLMSettings : ScriptableObject
-{
-    public string apiKey = "API key here";
-    public string selectedModel = "gpt-4o";
-}
